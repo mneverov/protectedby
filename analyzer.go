85e41c1631d78cc5e5a0341ba1743065686fd316
Max Neverov <neverov.max@gmail.com>
2024-04-09 10:16:39 +0200

    Add protected struct an intermediate container for the protected field, lock and others.

--- analyzer.go
+++ analyzer.go
@@ -2,9 +2,21 @@ package main
 
 import (
 	"flag"
+	"go/ast"
+	"go/token"
 	"golang.org/x/tools/go/analysis"
 	"golang.org/x/tools/go/analysis/passes/inspect"
 )
+
+const protectedBy = "protected by "
+
+type protected struct {
+	field           *ast.Field
+	lock            *ast.Field
+	containerStruct *ast.StructType
+	file            *ast.File
+	fset            *token.FileSet
+}
 
 var analyzer = &analysis.Analyzer{
 	Name:     "protectedby",
