ccad45dad8c23a46b7c00ee1bee9e9e5dc742e7d
Max Neverov <neverov.max@gmail.com>
2024-04-26 10:27:31 +0200

    Fix test explanations.

--- testdata/src/protectedby/blank.go
+++ testdata/src/protectedby/blank.go
@@ -3,8 +3,8 @@ package protectedby
 import "sync"
 
 type blankIdent struct {
-	// _ is protected by Mu. Make Mutex field public to make sure that it is not reported
-	// in case of embedded field.
+	// _ is protected by Mu. The mutex field Mu is intentionally public to make sure that it is not reported
+	// in case of blank identifiers.
 	_  int
 	Mu sync.Mutex
 }
--- testdata/src/protectedby/embedded.go
+++ testdata/src/protectedby/embedded.go
@@ -3,8 +3,8 @@ package protectedby
 import "sync"
 
 type embed struct {
-	// protected by Mu. Make Mutex field public to make sure that it is not reported
-	// in case of embedded field.
+	// protected by Mu. The mutex field Mu is intentionally public to make sure that it is not reported
+	// in case of embedded fields.
 	int
 	Mu sync.Mutex
 }
